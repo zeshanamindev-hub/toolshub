40282202917bd4ea764f6587c2c19696a22ed9bb
Zeshan Amin <zeshan.revnix@gmail.com>
2025-11-26 23:14:00 +0500

    Fix rorots.txt file

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -10,34 +10,14 @@ export default function robots(): MetadataRoute.Robots {
         disallow: [
           '/api/',
           '/admin/',
-          '/_next/',
           '/private/',
         ],
       },
       {
-        userAgent: 'Googlebot',
-        allow: '/',
-        disallow: ['/api/', '/admin/'],
-        crawlDelay: 1,
-      },
-      {
-        userAgent: 'Bingbot',
-        allow: '/',
-        disallow: ['/api/', '/admin/'],
-        crawlDelay: 1,
-      },
-      {
         userAgent: 'GPTBot',
-        allow: '/',
-        disallow: ['/api/', '/admin/'],
-      },
-      {
-        userAgent: 'ChatGPT-User',
-        allow: '/',
         disallow: ['/api/', '/admin/'],
       }
     ],
     sitemap: `${SITE_CONFIG.url}/sitemap.xml`,
-    host: SITE_CONFIG.url,
   }
 }
